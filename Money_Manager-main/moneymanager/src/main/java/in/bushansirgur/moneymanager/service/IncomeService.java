958c01f7f89931001248344d0c40457f698e45a5
Ujwal <ujwalswain64@gmail.com>
2026-06-05 17:04:16 +0530

    Fix deployed dashboard data and SPA routes

--- Money_Manager-main/moneymanager/src/main/java/in/bushansirgur/moneymanager/service/IncomeService.java
+++ Money_Manager-main/moneymanager/src/main/java/in/bushansirgur/moneymanager/service/IncomeService.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -21,6 +22,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class IncomeService {
     @Autowired
     private  CategoryRepository categoryRepository;
